ad8a56d24f9d5d1b3b6b83efe3f663d21f799d92
ElinVS <elinvsvennberg@gmail.com>
2022-04-24 10:23:53 +0100

    testing my select all function returns Alex and Benjamin from Databse and displays on webpage

--- controllers/owners_controller.py
+++ controllers/owners_controller.py
@@ -1,6 +1,13 @@
-from flask import Blueprint, Flask
+import re
+from flask import Blueprint, Flask, render_template
 
 from models.owner import Owner
+from repositories import owner_repository
 
 
 owners_blueprint = Blueprint("owners", __name__)
+
+@owners_blueprint.route('/')
+def owners():
+    owners = owner_repository.select_all()
+    return render_template('index.html', owners =owners)
--- repositories/owner_repository.py
+++ repositories/owner_repository.py
@@ -9,6 +9,18 @@ def save(owner):
     owner.id = id
 
 
+def select_all():
+    owners = []
+
+    sql = "SELECT * FROM owners"
+    results = run_sql(sql)
+
+    for result in results:
+        owner = Owner(result["first_name"],result["id"])
+        owners.append(owner)
+    return owners
+
+
 def delete_all():
     sql = "DELETE FROM owners"
     run_sql(sql)
